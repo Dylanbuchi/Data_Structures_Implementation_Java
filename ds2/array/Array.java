06fb59088beacd242abc1c91aae9d25b135f85eb
Dylan <devybuchi@gmail.com>
2020-06-03 10:05:27 -0300

    implemented a generic dynamic array

--- ds2/array/Array.java
+++ ds2/array/Array.java
@@ -4,73 +4,183 @@ import ds2.interfaces.LList;
 
 /**
  * Array
+ * 
+ * 
  */
-public class Array implements LList {
+@SuppressWarnings("unchecked")
+
+public class Array<T> implements LList<T> {
+
+    private int capacity;
+    private T[] array;
+    private int size;
+
+    public Array() {
+        this(16);
+    }
+
+    public Array(final int capacity) {
+
+        this.capacity = capacity;
+        this.array = (T[]) new Object[capacity];
+        this.size = 0;
+
+    }
+
+    private void resize(final int newCapacity) {
+        capacity = newCapacity;
+        final var copy = (T[]) new Object[capacity];
+
+        for (int i = 0; i < size; i++) {
+            copy[i] = array[i];
+
+        }
+        array = copy;
+
+    }
 
     @Override
     public int size() {
-        // TODO Auto-generated method stub
-        return 0;
+
+        return size;
     }
 
     @Override
     public int capacity() {
-        // TODO Auto-generated method stub
-        return 0;
+
+        return capacity;
     }
 
     @Override
     public boolean isEmpty() {
-        // TODO Auto-generated method stub
-        return false;
+
+        return size == 0;
     }
 
     @Override
-    public Object get(int index) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+    public T get(final int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayIndexOutOfBoundsException();
 
-    @Override
-    public void push(Object item) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void insert(int index, Object item) {
-        // TODO Auto-generated method stub
+        } else {
+            return array[index];
+        }
 
     }
 
     @Override
-    public void prepend(Object item) {
-        // TODO Auto-generated method stub
+    public void push(final T item) {
+
+        if (size == capacity) {
+            resize(capacity * 2);
+
+        }
+        array[size++] = item;
 
     }
 
     @Override
-    public Object pop() {
-        // TODO Auto-generated method stub
-        return null;
+    public void insert(final int index, final T item) {
+        if (isEmpty() && index == 0) {
+            array[index] = item;
+            size++;
+
+        } else if (index < 0 || index >= size) {
+            throw new ArrayIndexOutOfBoundsException();
+
+        } else if (item == null) {
+            throw new NullPointerException("The item to insert can't be null");
+
+        } else {
+            if (size == capacity) {
+                resize(capacity * 2);
+
+            }
+
+            for (int i = size; i > index; i--) {
+
+                array[i] = array[i - 1];
+
+            }
+
+            array[index] = item;
+            size++;
+
+        }
+
     }
 
     @Override
-    public Object delete(int index) {
-        // TODO Auto-generated method stub
-        return null;
+    public void prepend(final T item) {
+
+        insert(0, item);
+
     }
 
     @Override
-    public Object remove(Object item) {
-        // TODO Auto-generated method stub
-        return null;
+    public T pop() {
+        if (isEmpty()) {
+            throw new NullPointerException();
+        }
+        return array[size--];
     }
 
     @Override
-    public int find(Object item) {
-        // TODO Auto-generated method stub
-        return 0;
+    public T delete(final int index) {
+        if (isEmpty()) {
+            throw new NullPointerException();
+        }
+
+        else if (index < 0 || index >= size) {
+            throw new ArrayIndexOutOfBoundsException();
+        } else {
+            for (int i = index; i < size; i++) {
+                array[i] = array[i + 1];
+
+            }
+
+            T deleted = array[index];
+            size--;
+            return deleted;
+        }
+
+    }
+
+    @Override
+    public T remove(final T item) {
+        int index = 0;
+        while (true) {
+            index = find(item);
+            if (index == -1) {
+                break;
+
+            }
+            delete(index);
+        }
+        throw new NullPointerException();
+    }
+
+    @Override
+    public int find(final T item) {
+        for (int i = 0; i < size; i++) {
+            if (array[i] == item) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        var sb = new StringBuilder();
+
+        for (int i = 0; i < size; i++) {
+            sb = i == size - 1 ? sb.append("[" + array[i] + "]") : sb.append("[" + array[i] + "], ");
+
+        }
+
+        return sb.toString();
+
     }
 
 }
---
+++ ds2/array/ArrayApp.java
@@ -0,0 +1,25 @@
+package ds2.array;
+
+public class ArrayApp {
+
+    public static void main(String[] args) {
+        var a = new Array<Integer>(15);
+
+        System.out.println("capacity " + a.capacity());
+        a.prepend(232);
+        a.prepend(1);
+        a.prepend(2);
+        a.prepend(2);
+        a.prepend(2);
+        a.prepend(2);
+        a.prepend(2);
+        a.remove(2);
+
+        System.out.println(a);
+        System.out.println("capacity " + a.capacity());
+        System.out.println("size " + a.size());
+        System.out.println(a.get(0));
+
+    }
+
+}
